b6dcaa3f287f77b3aebcfc51ece9d7749df7d074
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-09-13 20:14:35 -0300

    Refatoração do Home Controller sem classe

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 
-//LL660974473CN
-export default class HomeController {
-  static async GetRastreioCorreios(codigoRastreio) {
-    let dadosRastreio;
+function GetObjetoRastreioByHtml(element) {
+  const itemArray = element.split('\n');
+  const itemObjeto = {
+    data: itemArray[0]
+      .split('<br>')
+      .join(' ')
+      .trim(),
+    local: itemArray[1]
+      .substring(itemArray[1].indexOf('>') + 1, itemArray[1].indexOf('</'))
+      .split('/')
+      .join(' ')
+      .trim(),
+    status: itemArray[2]
+      .substring(
+        itemArray[2].indexOf('g>') + 2,
+        itemArray[2].indexOf('</strong><br>'),
+      )
+      .trim(),
+    registro: itemArray[4].substring(0, itemArray[4].indexOf('</td>')).trim(),
+  };
 
-    await axios
+  return itemObjeto;
+}
+
+//LL660974473CN
+export default {
+  GetRastreioCorreios: async codigoRastreio => {
+    return await axios
       .get(`https://www.websro.com.br/detalhes.php?P_COD_UNI=${codigoRastreio}`)
       .then(function(response) {
         const indiceInicial = response.data.indexOf('<tbody>');
@@ -16,40 +38,10 @@ export default class HomeController {
         const tabelaArray = tabela.split("<tr><td valign='top'>");
         tabelaArray.shift();
 
-        let dataArray = [];
-
-        tabelaArray.forEach(element => {
-          const itemArray = element.split('\n');
-          const itemObjeto = {
-            data: itemArray[0]
-              .split('<br>')
-              .join(' ')
-              .trim(),
-            local: itemArray[1]
-              .substring(
-                itemArray[1].indexOf('>') + 1,
-                itemArray[1].indexOf('</'),
-              )
-              .split('/')
-              .join(' ')
-              .trim(),
-            status: itemArray[2]
-              .substring(
-                itemArray[2].indexOf('g>') + 2,
-                itemArray[2].indexOf('</strong><br>'),
-              )
-              .trim(),
-            registro: itemArray[4]
-              .substring(0, itemArray[4].indexOf('</td>'))
-              .trim(),
-          };
-
-          dataArray.push(itemObjeto);
-        });
-
-        dadosRastreio = dataArray;
+        return tabelaArray.map(GetObjetoRastreioByHtml);
+      })
+      .then(data => {
+        return data;
       });
-
-    return dadosRastreio;
-  }
-}
+  },
+};
