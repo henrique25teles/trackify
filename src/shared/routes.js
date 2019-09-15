fe35b440ebf4ecc426a225212cdcbc487449a781
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-09-15 02:22:23 -0300

    Mudança path das routes padrão para pasta shared

---
+++ src/shared/routes.js
@@ -0,0 +1,13 @@
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+const routes = {
+  Home: {
+    screen: Home,
+  },
+  About: {
+    screen: About,
+  },
+};
+
+export default routes;
