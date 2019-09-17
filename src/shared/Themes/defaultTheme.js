36505ec310bfa3cb5a7f32cd540c3315189f1a7d
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-09-16 23:41:15 -0300

    Criação de cores padrões de projeto. Ainda não implementadas.

---
+++ src/shared/Themes/defaultTheme.js
@@ -0,0 +1,24 @@
+const theme = {
+  Button: {
+    containerStyle: {
+      flexDirection: 'column',
+    },
+    titleStyle: {
+      color: 'black',
+    },
+    type: 'solid',
+    raised: true,
+  },
+  SearchBar: {
+    lightTheme: true,
+  },
+};
+
+const colors = {
+  enviromentColor: '#fafafa',
+  primaryColor: '#4096db',
+};
+
+export {};
+
+export default colors;
