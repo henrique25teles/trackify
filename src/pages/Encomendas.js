0ddd811cb8a72688f82303b7f80e6532de2e79da
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2019-09-19 21:02:55 -0300

    Update Encomendas.js
    Swipable de deletar inicio de aprendizado

--- src/pages/Encomendas.js
+++ src/pages/Encomendas.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, FlatList, RefreshControl} from 'react-native';
+import {View, Text, StyleSheet, FlatList, RefreshControl} from 'react-native';
 import Container from '../shared/components/Container';
 import {ListItem} from 'react-native-elements';
 import LeftAvatar from '../components/Encomendas/LeftAvatar';
@@ -7,6 +7,7 @@ import ListItemSeparator from '../shared/components/ListItemSeparator';
 import ListSearchHeader from '../shared/components/ListSearchHeader';
 import ButtonAdd from '../components/Encomendas/ButtonAdd';
 import EncomendaController from '../controllers/EncomendaController';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export default class Encomendas extends Component {
   constructor(props) {
@@ -22,21 +23,48 @@ export default class Encomendas extends Component {
 
   keyExtractor = (item, index) => index.toString();
 
+  renderRightDelete = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#ff0000',
+          height: '100%',
+        }}>
+        <Text
+          style={{
+            flex: 1,
+            color: '#fafafa',
+            fontSize: 30,
+          }}>
+          Deleta saporra
+        </Text>
+      </View>
+    );
+  };
+
   renderItem = ({item}) => {
     return (
       <Container>
-        <ListItem
-          title={item.Name}
-          subtitle={item.TrackingCode}
-          leftAvatar={LeftAvatar}
-          onPress={() =>
-            this.props.navigation.navigate('EncomendasDetalhes', {
-              data: item.Detalhes,
-            })
-          }
-          bottomDivider
-          chevron
-        />
+        <Swipeable
+          renderRightActions={this.renderRightDelete}
+          containerStyle={{
+            alignContent: 'flex-end',
+            justifyContent: 'flex-end',
+            flexDirection: 'column-reverse'}}>
+          <ListItem
+            title={item.Name}
+            subtitle={item.TrackingCode}
+            leftAvatar={LeftAvatar}
+            onPress={() =>
+              this.props.navigation.navigate('EncomendasDetalhes', {
+                data: item.Detalhes,
+              })
+            }
+            bottomDivider
+            chevron
+          />
+        </Swipeable>
       </Container>
     );
   };
